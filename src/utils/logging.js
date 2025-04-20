e077dc2b1c6791cc6e194762b4312ff4f6d5869d
Mireu Lim <limmireu1214@gmail.com>
2025-04-20 23:21:33 +0900

    move: 경로 변경

--- src/utils/logging.js
+++
@@ -1,9 +0,0 @@
-const chalk = require("chalk");
-
-module.exports = {
-	info: (message) => console.log(`${chalk.blue("[INFO]")} ${message}`),
-	warn: (message) => console.log(`${chalk.yellow("[WARNING]")} ${message}`),
-	error: (message) => console.log(`${chalk.red("[ERROR]")} ${message}`),
-	debug: (message) => console.log(`${chalk.green("[DEBUG]")} ${message}`),
-	music: (message) => console.log(`${chalk.cyan("[MUSIC]")} ${message}`),
-};
